775608fcce412b7f24b30121b904c393c9aa4791
Maria Eduarda Rodrigues <maria.rodrigues18@estudante.ifms.edu.br>
2023-08-31 08:19:32 -0400

    Inserindo props

--- src/componentes/filme/Filme.tsx
+++ src/componentes/filme/Filme.tsx
@@ -1,14 +1,18 @@
 import Imagem from "./../../assets/img/barbie-filme.png"
 import './Filme.css'
+type FilmeProps = {
+    titulo:string,
+    sinopse:string,
+    imagem:string
+}
 
-function Filme(){
+function Filme(props:FilmeProps){
     return(
             <div className="film-content">
-                {/* o comentário dentro do JSX é desse formato */}
                 <img src={Imagem} alt="" />
                 <div className="text-barbie">
-                    <h1>Barbie</h1>
-                    <p className="sinopse">Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.</p>
+                    <h1>{props.titulo}</h1>
+                    <p className="sinopse">{props.sinopse}</p>
                 </div>
             </div>
     )
